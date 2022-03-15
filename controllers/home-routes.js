629e5322e16af4ef6b2ac71beb183bc56f9e7107
yohans14 <yohans14@yahoo.com>
2022-03-15 07:08:21 -0500

    created partials and formated comment and likes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,6 +41,53 @@ router.get("/", (req, res) => {
 		});
 });
 
+router.get("/post/:id", (req, res) => {
+	Post.findOne({
+		where: {
+			id: req.params.id,
+		},
+		attributes: [
+			"id",
+			"content",
+			"title",
+			"created_at",
+			[
+				sequelize.literal(
+					"(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+				),
+				"vote_count",
+			],
+		],
+		include: [
+			{
+				model: Comment,
+				attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+				include: {
+					model: User,
+					attributes: ["username"],
+				},
+			},
+			{
+				model: User,
+				attributes: ["username"],
+			},
+		],
+	})
+		.then((dbPostData) => {
+			if (!dbPostData) {
+				res.status(404).json({ message: "No post found with this id" });
+				return;
+			}
+			const post = dbPostData.get({ plain: true });
+
+			res.render("single-post", { post, loggedIn: req.session.loggedIn });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
 router.get("/login", (req, res) => {
 	if (req.session.loggedIn) {
 		res.redirect("/");
@@ -49,4 +96,5 @@ router.get("/login", (req, res) => {
 
 	res.render("login");
 });
+
 module.exports = router;
