4f237e4e21557350061cf066d641587fc3e388c5
yohans14 <yohans14@yahoo.com>
2022-03-16 20:04:07 -0500

    fex bug

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require("./Vote");
-const Comment = require("./comment");
+const Comment = require("./Comment");
 
 // create associations
 User.hasMany(Post, {
