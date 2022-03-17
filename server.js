6bb4fb61f840963ca6e96264af77d880708166ee
yohans14 <yohans14@yahoo.com>
2022-03-16 19:51:29 -0500

    add secret

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
 const session = require("express-session");
+const { env } = require("process");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-	secret: "Super secret secret",
+	secret: process.env.secret,
 	cookie: {},
 	resave: false,
 	saveUninitialized: true,
